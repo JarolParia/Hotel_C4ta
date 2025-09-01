6cc8f7625825744a8cf032253f4e2f933c12debb
Jarol <jarolparia10@gmail.com>
2025-08-31 21:47:05 -0500

    Agregar archivos de proyecto.

---
+++ View/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotel_C4ta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Join_Click(object sender, RoutedEventArgs e)
+        {
+            // Lógica de autenticación aquí
+            // Ejemplo: MessageBox.Show("Botón JOIN pulsado");
+        }
+    }
+}
